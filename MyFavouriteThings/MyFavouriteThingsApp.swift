6e30e484151c239a9937dfca070f8b871a33d2ac
Joseph Ji <jji@lcs.on.ca>
2024-11-22 08:42:07 -0500

    Initial Commit

---
+++ MyFavouriteThings/MyFavouriteThingsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyFavouriteThingsApp.swift
+//  MyFavouriteThings
+//
+//  Created by Xinchen Ji on 2024-11-22.
+//
+
+import SwiftUI
+
+@main
+struct MyFavouriteThingsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
